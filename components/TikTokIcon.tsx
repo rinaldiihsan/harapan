e2ebc729bf88904582b24386240a3c6796336ddb
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2026-03-02 14:03:30 +0700

    fix: tiktok icon

--- components/TikTokIcon.tsx
+++ components/TikTokIcon.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
+import Image from 'next/image';
 
-export const TikTokIcon = ({ size = 24, color = 'currentColor' }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill={color}>
-    <path d="M19.589 6.686a4.793 4.793 0 0 1-3.77-1.879V13.3a5.32 5.32 0 1 1-5.32-5.32c.186 0 .37.01.551.03v2.736a2.588 2.588 0 1 0 2.588 2.588V2h2.736a4.79 4.79 0 0 0 3.215 3.215v1.471z" />
-  </svg>
-);
+export const TikTokIcon = ({ size = 24, color = '#047038' }) => <Image src="/tiktok-svgrepo-com.svg" alt="TikTok Icon" width={size} height={size} style={{ color }} />;
