d4484db25e1042d38c44d8406c1c620ab0e687ce
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2026-05-30 10:58:51 +0700

    init: just update package

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require('http');
+const next = require('next');
+
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
